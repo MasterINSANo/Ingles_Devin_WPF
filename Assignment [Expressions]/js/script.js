5cbc4688b78d6ee21cf9e613c3138edaaa9c591a
MasterINSANo <merlinssondemi@msn.com>
2014-11-30 17:32:34 -0600

    Arithmetic Operators
    Working with the Arithmetic Operators video

--- Assignment [Expressions]/js/script.js
+++ Assignment [Expressions]/js/script.js
@@ -1,8 +1,8 @@
-//Expressions - Age Example
+//Expressions - Arithmetic Operators
 
-var yearBorn = 1987;
-var age = 2014 - yearBorn - 1;
-console.log(age);
-
-
+//area of the triangle is half of the width times the height
+var width = 8;
+var height = 7;
+var area = width * height/2;
+console.log(area)
 
