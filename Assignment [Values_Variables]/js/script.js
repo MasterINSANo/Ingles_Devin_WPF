f54c564e0ad99d82ffe12510af85705b5b48ffd3
MasterINSANo <merlinssondemi@msn.com>
2014-11-30 16:27:19 -0600

    Defining Variables
    Watching Defining Variables Video and trying out.

--- Assignment [Values_Variables]/js/script.js
+++ Assignment [Values_Variables]/js/script.js
@@ -1,3 +1,8 @@
-// JavaScript
+// Defining Variables
 
-var age;
+var age; //declaration
+age = 27; //definition
+
+//storage container = value
+
+var yearOfBirth = 1987; //declaring and defining on the same line
