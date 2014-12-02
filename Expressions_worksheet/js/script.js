62c46c40b721abfaaf7476d003fbcb43ee34eaa4
MasterINSANo <merlinssondemi@msn.com>
2014-12-01 21:00:59 -0600

    Dog Years
    editing Dog Years adding comments.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,8 +1,8 @@
 //Expression Worksheet
 
 //Dog Years
-var humanToDog = 7;
-var sparky = prompt("How old is Sparky");
-var total = humanToDog * sparky;
-alert("Sparky is " + sparky + " human years old which is " + total + " in dog years");
-console.log(total);
+var humanToDog = 7; //change variable
+var sparky = prompt("How old is Sparky"); //input to find out Sparky's human age
+var total = humanToDog * sparky; //calculates the human age of sparky to his actual age in dog years
+alert("Sparky is " + sparky + " human years old which is " + total + " in dog years"); //answer to how old sparky is in dog years
+console.log(total); //checking answer
