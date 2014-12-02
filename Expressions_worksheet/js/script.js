78f130980887b265c6c1c433db7522a3921db607
MasterINSANo <merlinssondemi@msn.com>
2014-12-01 20:55:56 -0600

    Dog Years
    My end result in Dog Years worksheet

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,8 @@
+//Expression Worksheet
+
+//Dog Years
+var humanToDog = 7;
+var sparky = prompt("How old is Sparky");
+var total = humanToDog * sparky;
+alert("Sparky is " + sparky + " human years old which is " + total + " in dog years");
+console.log(total);
