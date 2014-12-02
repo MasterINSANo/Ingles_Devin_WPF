c5cf8f4a4ebed1eaaa74172461168c58d83f0171
MasterINSANo <merlinssondemi@msn.com>
2014-12-01 22:04:11 -0600

    Slice of Pie Part2
    Working on worksheet slice of pie part 2

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,12 +1,10 @@
 //Expression Worksheet
 
-//Slice of Pie Part 1
+//Slice of Pie Part 2
 
 var slice = prompt("how many slices of pizza per box?"); //enter in how many slices are in the box
 var people = prompt("how many people are at the party?"); //tell how many people were at the party
 var order = prompt("how many pizza's were ordered?"); //how many pizzas were ordered
+var total = slice * order % people; //total of slices per person with the remainder going to sparky
 
-var total = slice * order / people; //total of slices per person
-
-alert("Each person ate " + total + " slices of pizza at the party");
-console.log(total);
+alert("Sparky got " + total + " slices of pizza");
