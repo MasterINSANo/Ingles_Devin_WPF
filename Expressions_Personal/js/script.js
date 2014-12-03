ec90ae8a07265e65a2f3609f0e76eee716aa520d
MasterINSANo <merlinssondemi@msn.com>
2014-12-02 23:45:20 -0600

    different route
    Trying a math by follow approch. asking questions, and giving answers

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,12 +4,12 @@
 //I am using my last two weeks as basis for tips earned
 
 var tips = [37, 160, 163, 159]; //I work 2 nights a week
-
+alert("If I made $" + tips + " in four nights that I worked, what is the average that I made?")
 var total = tips[0] + tips[1] + tips[2] + tips[3]; //taking the 4 nights in the last two weeks
 
 var avg = total / 4; //taking the total and dividing by 4
 
 console.log(avg);
 
-alert("I make on average $129.75 in tips working four nights in two weeks.\n What is the average if you made $100, $67, $83, and $75?");
+alert("Correct!!\n I made on average $129.75 in tips working four nights in two weeks.\n What is the average if you made $100, $67, $83, and $75?"); //Seeing if user can match same answer I get
 
