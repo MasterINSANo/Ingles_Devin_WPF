8d92819e765f0c039d45c60f09767e7a35222a16
MasterINSANo <merlinssondemi@msn.com>
2014-12-03 12:38:03 -0600

    Getting deeper
    Can the machine trust your input? hehe

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -8,3 +8,14 @@ alert("Are you sure?");
 
 yearBorn = prompt("What Year were you born?");
 
+alert("So you are telling me that you were born in " + yearBorn + "?");
+
+alert("I don't believe you.");
+
+onlyYou = prompt("If you were born in " + yearBorn + ", tell me something that happened only you would know.");
+
+lies = prompt("How can I believe you? Only you would know what happened. You could be making it up!\n So if, " + onlyYou + " really happened, then what is your age?");
+
+age = 2014 - yearBorn;
+
+alert("So you are telling me, that you are " + age + " Years of age?\n Wow you are super old!");
