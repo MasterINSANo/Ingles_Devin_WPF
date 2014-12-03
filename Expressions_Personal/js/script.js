44eb9c62b755bfa0651e955cf0c5ffb4661f77a0
MasterINSANo <merlinssondemi@msn.com>
2014-12-02 22:46:03 -0600

    starting Code
    I am using my last two weeks as a basis for this assignment.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,4 +1,6 @@
 //Devin Ingles - 12/02/2014 - Expressions_Personal
 
 //Average tips per night, working as Pizza Delivery
+//I am using my last two weeks as basis for tips earned
 
+var tips = [37, 160, 163, 159]
