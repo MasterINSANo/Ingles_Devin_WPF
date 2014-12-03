82ee395edfe8c02d78d0ea1fc97fcac3625b7034
MasterINSANo <merlinssondemi@msn.com>
2014-12-03 09:10:21 -0600

    Adding comments
    Added comments to show my train of thought, also getting ready to add
    end game

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,10 +2,15 @@
 
 //How much commission I would make if I made 8% of my total.
 
-var smartPhone = 300;
+var smartPhone = 300; //How much profit made on selling a Smart Phone
 
-var basicPhone = 150;
+var basicPhone = 150; //How much profit made on selling a basic phone
 
-var insurance = 129.99;
+var insurance = 129.99; //if both phones bought insurance full profit of 129.99
 
-var total = smartPhone + basicPhone + insurance * 2;
+var total = smartPhone + basicPhone + insurance * 2; //Both phones adding insurance, then adding total together
+
+console.log("I made a total of $" + total);
+
+var commission = total * 
+
