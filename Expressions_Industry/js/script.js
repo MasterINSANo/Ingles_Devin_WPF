47a024c3ad44262b17bdf970072ca606e7e48789
MasterINSANo <merlinssondemi@msn.com>
2014-12-03 09:04:45 -0600

    Mathing
    adding different numbers together checking my math

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,2 +1,11 @@
 //Devin Ingles 12/03/2014 Expressions_Industry
 
+//How much commission I would make if I made 8% of my total.
+
+var smartPhone = 300;
+
+var basicPhone = 150;
+
+var insurance = 129.99;
+
+var total = smartPhone + basicPhone + insurance * 2;
