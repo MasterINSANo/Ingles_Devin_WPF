696f4bb8c5da29c9551b0044f4ca9af76b79c563
MasterINSANo <merlinssondemi@msn.com>
2014-12-02 22:56:12 -0600

    adding comments
    just adding comments of my thought process

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,10 +3,10 @@
 //Average tips per night, working as Pizza Delivery
 //I am using my last two weeks as basis for tips earned
 
-var tips = [37, 160, 163, 159];
+var tips = [37, 160, 163, 159]; //I work 2 nights a week
 
-var total = tips[0] + tips[1] + tips[2] + tips[3];
+var total = tips[0] + tips[1] + tips[2] + tips[3]; //taking the 4 nights in the last two weeks
 
-var avg = total / 4;
+var avg = total / 4; //taking the total and dividing by 4
 
 console.log(avg);
