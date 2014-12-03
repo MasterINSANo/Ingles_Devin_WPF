0c4a06bc41957c29f6081e9f876e7db8843c2cac
MasterINSANo <merlinssondemi@msn.com>
2014-12-03 00:04:01 -0600

    Almost Done
    Finalizing project. There is tons of Math and Alerts.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -13,11 +13,19 @@ alert("Add the total of the four different tips made \n Then divide by the numbe
 
 var avg = total / 4; //taking the total and dividing by 4
 
-console.log(avg);
+console.log(avg); //checking average
 
 alert("Correct!!\n I made on average $129.75 in tips working four nights in two weeks.\n What is the average if you made $100, $67, $83, and $75?"); //Seeing if user can match same answer I get
 
-var yourTips = [100, 67, 83, 75];
+var yourTip = [100, 67, 83, 75];
 
-alert("Remember, add the four numbers then divide by how many numbers there are.")
+var yourTipTotal = yourTip [0] + yourTip[1] + yourTip[2] + yourTip[3];
+
+var yourTipAvg = yourTipTotal /4;
+
+console.log(yourTipAvg);
+
+alert("Remember, add the four numbers then divide by how many numbers there are.");
+
+alert("Correct! \n If your answer was ")
 
