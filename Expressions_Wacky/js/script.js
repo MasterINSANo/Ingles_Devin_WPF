81bd6d6e1ec0033922b781ff2cab1232907a0a32
MasterINSANo <merlinssondemi@msn.com>
2014-12-03 12:21:03 -0600

    Something Silly
    Trying a silly way to wacky expressions.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,10 @@
 //Devin Ingles 12/03/2014 Expression_Wacky
 
+alert("Warning, what you are about to see can hurt your eyes.");
+
+yes = prompt("Are you 18? Please type Yes or No");
+
+alert("Are you sure?");
+
+yearBorn = prompt("What Year were you born?");
 
