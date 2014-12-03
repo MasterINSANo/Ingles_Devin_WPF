3dce26923c426ad6cf92ef777ff08e146c5f56c1
MasterINSANo <merlinssondemi@msn.com>
2014-12-02 23:00:00 -0600

    Adding question
    Adding, what if question to make user interaction

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -9,4 +9,7 @@ var total = tips[0] + tips[1] + tips[2] + tips[3]; //taking the 4 nights in the 
 
 var avg = total / 4; //taking the total and dividing by 4
 
-console.log(avg);
+console.log(avg)
+
+alert("I make on average $129.75 in tips working two nights a week. What would I make if there was no holiday season, and it was slower?") // Asking question to start interaction
+
