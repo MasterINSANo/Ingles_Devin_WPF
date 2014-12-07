7546bf73119d07f494c2882e82d79ab146be1af3
MasterINSANo <merlinssondemi@msn.com>
2014-12-07 12:51:08 -0600

    Logical Operators
    Still working with the LO video using the ||

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,11 +1,11 @@
 //Conditional Logic - Logical Operators
 
-var budget = 300;
+var budget = 100;
 var samsungPrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
 //if the price of the phone s less our budget AND if our paycheck is over 300
-if(samsungPrice < budget && paycheck > 300){
+if(samsungPrice < budget || wonLottery === true){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you")
