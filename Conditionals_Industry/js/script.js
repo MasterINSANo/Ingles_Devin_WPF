04d9f76d63ad1e4e9e5bd70f8675e7354131f227
MasterINSANo <merlinssondemi@msn.com>
2014-12-08 17:42:18 -0600

    Conditionals Industry start
    Starting on writing up selling insurance for a smartphone

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,1 +1,7 @@
 //Devin Ingles 12/08/2014 Conditionals_Industry
+
+//
+
+var device = prompt("What phone Operating system to you have between: Apple, Android, or Windows?");
+alert("Did you know about our in store Insurance?\n It covers everything from Lost and Stolen to Water/Physical Damage.");
+var insurance = prompt("Would you like to add the in store insurance?\nPlease enter Yes or No.");
