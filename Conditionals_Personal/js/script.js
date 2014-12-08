372107c23beb5f20389fbb786c30ff73fa9b0806
MasterINSANo <merlinssondemi@msn.com>
2014-12-08 17:34:50 -0600

    Final
    Reworded a few prompts and changed some other little things.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,11 +2,12 @@
 
 //Good grades get us places!
 
-var grade = prompt("What is your ending grade?"); //I am wanting to know what grade they received at the end of the year.
-var reward = prompt("Where would you like to go?"); //I want to see where they want to go so that they can be rewarded for doing good in school.
+var grade = prompt("What is your final grade at the end of the year?"); //I am wanting to know what grade they received at the end of the year.
+var reward = prompt("Where would you like to go as a reward for getting good grades?"); //I want to see where they want to go so that they can be rewarded for doing good in school.
+
 //If your grade is equal to or greater then 70%, then you get to go to your reward.
 if (grade >= 70){
-        console.log("Congratulations! You can go to " + reward + ".");
+        console.log("Congratulations! You can go to " + reward + "!");
 }else{
     console.log("Sorry, but because you got a " + grade + "%, you cannot go to " + reward + ".");
     }
