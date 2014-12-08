1e43b4b111aa87b3ccc1a6f25adeb0ea64ec7c49
MasterINSANo <merlinssondemi@msn.com>
2014-12-08 17:50:34 -0600

    multiple choice
    Trying to get 3 different answers out of what phone they use.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,6 +2,13 @@
 
 //
 
-var device = prompt("What phone Operating system to you have between: Apple, Android, or Windows?");
+var device = prompt("What phone Operating system do you use between: Apple, Android, or Windows?");
 alert("Did you know about our in store Insurance?\n It covers everything from Lost and Stolen to Water/Physical Damage.");
 var insurance = prompt("Would you like to add the in store insurance?\nPlease enter Yes or No.");
+
+if(device === "Apple" || device === "apple"){
+    console.log("If your phone is an " + device + ", then the insurance is a one time of $149.99!")
+    if(device === "Android" || device === "android"){
+        console.log("If your Phone is an " + device + ",")
+}
+}
