414a16e276a3ee2d3a7702375eae54c103537ada
MasterINSANo <merlinssondemi@msn.com>
2014-12-08 15:44:58 -0600

    simple code.
    I believe this to be too simple, so I am going to work and add a few
    other options.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,10 +1,10 @@
 //Devin Ingles 12/08/2014 Conditionals_Personal
 
 var grade = prompt("What is your ending grade?");
-var reward = ("Where would you like to go?");
+var reward = prompt("Where would you like to go?");
 
 if(grade >= 70){
     console.log("Congratulations! You can go to " + reward + ".");
 }else{
-    console.log()
+    console.log("Sorry, but because you got a " + grade + ", you cannot go to " + reward + ".")
 }
