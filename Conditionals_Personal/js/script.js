76a19bd964399792ae47b444a91b08a3cbcca575
MasterINSANo <merlinssondemi@msn.com>
2014-12-08 17:24:27 -0600

    Adding to problem
    Working on trying to get the problem more complex.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,9 +2,11 @@
 
 var grade = prompt("What is your ending grade?");
 var reward = prompt("Where would you like to go?");
+var behavior = prompt("Where you Good or Bad in school?")
 
-if(grade >= 70){
-    console.log("Congratulations! You can go to " + reward + ".");
-}else{
-    console.log("Sorry, but because you got a " + grade + ", you cannot go to " + reward + ".")
-}
+if (grade >= 70){
+    if(behavior === "Good" || behavior === "good"){
+        console.log("Congratulations! You can go to " + reward + ".");
+    }else{
+    console.log("Sorry, but because you got a " + grade + "%, you cannot go to " + reward + ".");
+    }
