811c3d9c4902be4db08c4762f57c2b8687f6ea65
MasterINSANo <merlinssondemi@msn.com>
2014-12-07 23:14:34 -0600

    Movie Ticket Price
    Working ion the conditionals worksheet group 3, Multiple Conditions -
    Movie Ticket Price

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,16 +1,13 @@
-//Devin Ingles 12/07/14 Conditionals_Worksheet - Multiple Results - Check the Login
+//Devin Ingles 12/07/14 Conditionals_Worksheet - Multiple Conditions - Movie Ticket Price
 
-var userName = "Bananaiscool";
-var userInput = prompt("Please log in with your Username");
-var password = "Pa55word";
-var passwordInput = prompt("Please enter in your password");
-
-if(userInput == userName){
-    if(passwordInput == password){
-        console.log("Welcome, " + userName + "!");
-    }else{
-        console.log("Password dose not match our records.");
-   }
+var ticketPrice = 12;
+var discountPrice = 7;
+var  age = prompt("How old is the customer?");
+var time = prompt("What time is it?");
+//if the age is equal to or less then 10, or equal to or greater then 55 or if they watch between 3 and 5, they get the $7 discount.
+if(age <= 10 || age >= 55 || time >= 3 && time <= 5){
+    console.log("Your ticket price is $" + discountPrice + ".");
 }else{
-    console.log("User not found. Try again.");
-}   
+    console.log("Your ticket price is $" + ticketPrice + ".");
+}
+//If they do not meet the required statements above, the ticket price is $12.
