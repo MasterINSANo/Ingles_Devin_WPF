af0a54be260f2b0823612a46ecffcd6260416d6d
MasterINSANo <merlinssondemi@msn.com>
2014-12-08 18:44:16 -0600

    Adding ||
    Changed the && to ||. Started to get my result until I noticed that
    when I would pick an uppercase letter with a lower case, it would not
    register. Only would register if both were upper or lower.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,12 +6,12 @@ var device = prompt("What phone Operating system do you use between: Apple, Andr
 alert("Did you know about our in store Insurance?\n It covers everything from Lost and Stolen to Water/Physical Damage.");
 var insurance = prompt("Would you like to add the in store insurance?\nPlease enter Yes or No.");
 
-if(device === "Apple" && insurance === "Yes" || device === "apple" && insurance === "yes") {
+if(device === "Apple" || insurance === "Yes" || device === "apple" || insurance === "yes") {
     console.log("If your phone is an " + device + " device, then the insurance is a one time of $149.99!");
-}else if(device === "Android" && insurance === "Yes" || device === "android" && insurance === "yes"){
+}else if(device === "Android" || insurance === "Yes" || device === "android" || insurance === "yes"){
     console.log("If your phone is an " + device + " device, then the insurance is a one time of $129.99!");
-}else if(device === "Windows" && insurance === "Yes" || device === "windows" && insurance === "yes"{
-    console.log()
+}else if(device === "Windows" || insurance === "Yes" || device === "windows" || insurance === "yes"){
+    console.log("If your phone is a " + device + " device, then the insurance is a one time of $99.99!");
 }else{
-    
+    console.log("I always recommend insurance on any device, but if you are sure that you do not want insurance we can proceed to checkout.");
 }
