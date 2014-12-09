e78e0ea0ec38a34d2ce81655fae1016665b15626
MasterINSANo <merlinssondemi@msn.com>
2014-12-08 18:11:00 -0600

    working on line two
    I am working on the 2nd answer. going to try add something to see where
    i get from there.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,11 +6,12 @@ var device = prompt("What phone Operating system do you use between: Apple, Andr
 alert("Did you know about our in store Insurance?\n It covers everything from Lost and Stolen to Water/Physical Damage.");
 var insurance = prompt("Would you like to add the in store insurance?\nPlease enter Yes or No.");
 
-if(device == "Apple" && insurance == "Yes" || device == "apple" && insurance == "yes"){
-    console.log("If your phone is an " + device + ", then the insurance is a one time of $149.99!");
-    if(device == insurance == "Android" || device == insurance == "android"){
-        console.log("If your Phone is an " + device + ", then the insurance is a one time of $129.99!");
-}else{
-      console.log("If your Phone is an " + device + ", then the insurance is a one time of $99.99!");
+if(device === "Apple" && insurance === "Yes" || device === "apple" && insurance === "yes"){
+    console.log("If your phone is an " + device + " device, then the insurance is a one time of $149.99!");
+    if(device === "Android" && insurance === "Yes" || device === "android" && insurance === "yes"){
+        console.log("If your phone is an " + device + " device, then the insurance is a one time of $129.99")
+        if(device ==){
+
+        }
     }
 }
