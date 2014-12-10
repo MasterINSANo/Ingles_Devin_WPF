3472c94920b84e2571cb44de310a8a4384a95eae
MasterINSANo <merlinssondemi@msn.com>
2014-12-09 18:53:56 -0600

    Conditionals Wacky
    Working on the first steps to a wacky assignment.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,1 +1,6 @@
 //Devin Ingles 12/08/2014 Conditionals_Wacky
+
+var timeNow = prompt("W");
+var time;
+
+time = (timeNow > 9) ? "You should take a break and get some rest!!" : "Yeah, we still have time to work on homework!";
