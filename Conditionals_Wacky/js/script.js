4e72e56be779ffdd050c91d6f5d90546763175ba
MasterINSANo <merlinssondemi@msn.com>
2014-12-11 10:04:19 -0600

    Final
    Changed some wording, and added one more comment.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,13 +1,14 @@
 //Devin Ingles 12/08/2014 Conditionals_Wacky
 
-var job = prompt("Where do you work?"); //not needed to make decision, but to produce output.
+var job = prompt("What company do you work for?"); //not needed to make decision, but to produce output.
 var hourly = prompt(" How much do you make an Hour?\n (Rounded Up)"); //not needed to make decision, but to produce output.
-var raise = prompt("How many years have you been at your job?"); //needed to make a Ternary
+var raise = prompt("How many years have you been at " + job + "?"); //needed to make a Ternary
 //You will receive a raise if you have worked for the company for 2 years or more. You will not receive a raise if you have worked less then 2 years.
+
 raiseMade = (raise >= 2) ? "Congratulations, you get a raise!" : "Unfortunately, you have not worked at the company long enough for a raise.";
 console.log(raiseMade); //Printed out to console true or False answer
 
-if(raise >= 2){
+if(raise >= 2){ //added if else to alert the user of the decision.
     alert("Because you have worked for " + job + " for " + raise + " years, you get a $1 raise added to your $" + hourly + " an hour.");
 }else{
     alert("You have only been with " + job + " for less then 2 years, I am sorry, but you do not receive a raise.");
