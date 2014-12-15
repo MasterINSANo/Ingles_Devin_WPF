dbd5e29e75e0bd40309fbea8ba4d67d6059881b9
MasterINSANo <merlinssondemi@msn.com>
2014-12-14 22:35:06 -0600

    Do While Loop
    Working with the Do While Loop screen cast

--- Loops/js/script.js
+++ Loops/js/script.js
@@ -1,4 +1,4 @@
-//While Loop
+//Do While Loop
 console.log('-----Loops--------');
 
 var b = 10; //setups the index
@@ -7,3 +7,14 @@ while (b > 0){ // checks the condition
     console.log(b + "kegs on the wall");
     b--; //increments or decrements the index
 }
+
+console.log("------- Do While Loops --------");
+
+
+var c = 10;
+
+do{
+    console.log(c + " Kegs on the wall");
+    c--;
+}
+while(c > 0);
