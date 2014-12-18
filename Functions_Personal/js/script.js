14a0392e7438147ebea12a0df6d3558492db3327
MasterINSANo <merlinssondemi@msn.com>
2014-12-17 21:19:10 -0600

    finishing comments
    Added the rest of my comments, now trying to see about adding some more
    to the problem.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -6,9 +6,9 @@ var naps = prompt("How many naps did your baby take during the day?"); //Adding 
 console.log(naps + " Nap(s).");
 var napLength = prompt("What is the average time your baby slept in minutes, per nap?"); //seeing how long the baby sleeps through naps, determines if they would be able to sleep at 8 or 9pm.
 console.log("My baby slept an average of " + napLength + " Minutes per nap.");
-var time;
-
-time = (naps >= 3 || napLength >= 60) ? "The Best time to put the baby to bed would be 9PM" : "You should lay them down to sleep at 8pm.";
-console.log(time);
+var time; //Setting up for a ternary
+//If your baby sleeps for 3 or more naps a day, a later time to send them to bed is preferred. Or even if you lay them down for a single nap, and they sleep longer 60 minutes or more, then 9pm would be a good time to lay them down. Otherwise they will be tired and you can lay them down at 8pm.
+time = (naps >= 3 || napLength >= 60) ? "The Best time to put the baby to bed would be 9PM" : "You should lay them down to sleep at 8pm."; //added a logical operator to decide one or the other.
+console.log(time); //printing answer to console.log
 
 
