bd74daba679ca69b6fad72d6f7af31011bc9b32d
MasterINSANo <merlinssondemi@msn.com>
2014-12-17 22:45:20 -0600

    Changing wording add Parameters
    Working on changing how some things are worded in prompts and getting
    ready to set up for parameters

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,12 +5,15 @@
 var guest = prompt("How many guests will you have at your party?");
 console.log("You have " + guest + " guests coming.");
 var pizza = 3.5
+alert("One Pizza feeds about 3 and a half (3.5) people.");
 
 var pizzaPerGuest = function(guest, pizza){
     var totalPizza = guest / pizza;
     return totalPizza;
 }
 
-var total = pizzaPerGuest(guest, 3.5);
+var total = pizzaPerGuest(guest, pizza);
 
-console.log(total);
+console.log("You will need a total of " + total + " Pizza(s) for your party.");
+
+alert("You only invited " + guest + " guests, but they all have a +2.\nHow many more extra pizzas do you need to order?");
