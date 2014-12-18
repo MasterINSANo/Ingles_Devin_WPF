63ed3b945d5aabf6fba243f5a0f8effd44e53991
MasterINSANo <merlinssondemi@msn.com>
2014-12-17 22:56:27 -0600

    parameters a go
    I got the formula working correctly.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,7 +2,7 @@
 
 //How much Pizza is needed for your party??
 
-var guest = prompt("How many guests will you have at your party?");
+var guest = prompt("How many guests did you invite to your party?");
 console.log("You have " + guest + " guests coming.");
 var pizza = 3.5
 alert("One Pizza feeds about 3 and a half (3.5) people.");
@@ -14,6 +14,12 @@ var pizzaPerGuest = function(guest, pizza){
 
 var total = pizzaPerGuest(guest, pizza);
 
-console.log("You will need a total of " + total + " Pizza(s) for your party.");
+console.log("You will need a total of " + total + " Pizza(s) for the guests that you invited.");
 
-alert("You only invited " + guest + " guests, but they all have a +2.\nHow many more extra pizzas do you need to order?");
+alert("You only invited " + guest + " guests, but they all have a +2.\nHow many total pizzas do you need to order?");
+
+function extraPizza(pizza){
+    var extraPizza = (guest * 3) / 3.5;
+    console.log("You will need to order a total of " + extraPizza + " Pizzas for all guests.");
+}
+extraPizza();
