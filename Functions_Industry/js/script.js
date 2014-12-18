b320b45c91e8a68bdbf8320dbc7d47da29b64f69
MasterINSANo <merlinssondemi@msn.com>
2014-12-17 23:51:13 -0600

    Setting up Functions_Wacky
    Set up project and getting a good start with it.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,12 +2,12 @@
 
 //How much Pizza is needed for your party??
 
-var guest = prompt("How many guests did you invite to your party?");
+var guest = prompt("How many guests did you invite to your party?"); //asking for user input on how many guest are coming
 console.log("You have " + guest + " guests coming.");
-var pizza = 3.5
+var pizza = 3.5 //One pizza feeds 3 and a half people
 alert("One Pizza feeds about 3 and a half (3.5) people.");
 
-var pizzaPerGuest = function(guest, pizza){
+var pizzaPerGuest = function(guest, pizza){ //anonymous function to figure total of how many pizzas needed to order fr you invited guests
     var totalPizza = guest / pizza;
     return totalPizza;
 }
@@ -16,10 +16,10 @@ var total = pizzaPerGuest(guest, pizza);
 
 console.log("You will need a total of " + total + " Pizza(s) for the guests that you invited.");
 
-alert("You only invited " + guest + " guests, but they all have a +2.\nHow many total pizzas do you need to order?");
+alert("You only invited " + guest + " guests, but they all have a +2.\nHow many total pizzas do you need to order?"); //Just like any good party, your guests brought 2 friends with them.
 
 function extraPizza(pizza){
     var extraPizza = (guest * 3) / 3.5;
-    console.log("You will need to order a total of " + extraPizza + " Pizzas for all guests.");
+    console.log("You will need to order a total of " + extraPizza + " Pizzas for all guests."); //added a parameter to come up with the total for all pizzas needing to be ordered.
 }
-extraPizza();
+extraPizza(); //calling the answer to the parameter.
---
+++ Functions_Wacky/js/script.js
@@ -0,0 +1,21 @@
+//Devin Ingles 12/17/14 Functions_Wacky
+
+
+
+alert("It is a cold a stormy night. You are walking in the darkness until you stumble across a legendary sword called Excalibur.");
+var sword = prompt("It is wedged into a massive rock, Do you try to pull out the sword?(Yes or No)");
+
+if(sword === "Yes"){
+    console.log("You are asked a question involving math, and your life!");
+}else if(sword === "No"){
+    console.log("You are ambushed by Robot Dinosaurs!\n Run for your life!!!");
+}else{
+    console.log("You decide to keep walking, magical things can be dangerous!");
+}
+
+if(sword === "Yes"){
+    math = prompt("If 5*5 is 25, what would be the opposite of 25*25?");
+    console.log("The Correct answer is 5! The opposite of 25*25 is 25/25, with the answer being 5!");
+}else if(sword === "no"){
+    console.log("A Robot Dinosaur cuts off your escape and then leaps toward you with mouth wide open, growling, with teeth sharp as a knife!");
+}
