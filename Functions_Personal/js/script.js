ecf136936427b8b39ceb000d71eda5ea356d6839
MasterINSANo <merlinssondemi@msn.com>
2014-12-17 20:47:54 -0600

    Baby Sleeps
    Setting up user prompt information asking time baby took naps and how
    long in minutes.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,2 +1,8 @@
 // Devin Ingles 12/17/14 Functions_Personal
 
+//What time should you lay your baby down for bed.
+
+var naps = prompt("How many naps did your baby take during the day?");
+console.log(naps + " Nap(s).");
+var napLength = prompt("What is the average time your baby slept in minutes, per nap?");
+console.log("My baby slept an average of " + napLength + " Minutes per nap.");
