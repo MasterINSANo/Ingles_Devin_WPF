fb765cfad71dc7e68cd57512f214e6510d66c638
MasterINSANo <merlinssondemi@msn.com>
2014-12-17 21:01:37 -0600

    adding logical operator
    Added a logical operator into the formula.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -6,3 +6,9 @@ var naps = prompt("How many naps did your baby take during the day?");
 console.log(naps + " Nap(s).");
 var napLength = prompt("What is the average time your baby slept in minutes, per nap?");
 console.log("My baby slept an average of " + napLength + " Minutes per nap.");
+var time;
+
+time = (naps >= 3 || napLength >= 60) ? "The Best time to put the baby to bed would be 9PM" : "You should lay them down to sleep at 8pm.";
+console.log(time);
+
+
