8b792c5d6abbb406c5b14cf27b46f7f53deb81d1
MasterINSANo <merlinssondemi@msn.com>
2014-12-17 22:23:05 -0600

    Arithmetic Operators
    I am working on adding some Arithmetic Operators. Trying to figure out
    best way to do so.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,2 +1,8 @@
 //Devin Ingles 12/17/14 Functions_Industry
 
+//How much Pizza is needed for your party??
+
+alert("One pizza feeds up to 3.5 guests.");
+var guest = prompt("How many guests will you have at your party?");
+console.log("You have " + guest + " guests coming.");
+var pizza = 
